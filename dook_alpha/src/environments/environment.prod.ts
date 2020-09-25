3a30a4028b15ab085cf6909ce4caa40543787286
DookBeta <67843606+DookBeta@users.noreply.github.com>
2020-09-25 10:59:53 -0500

    Update environment.prod.ts

--- dook_alpha/src/environments/environment.prod.ts
+++ dook_alpha/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCMXmqJWVnOhyfn8a_vbSQz6I_pb0EvUe4",
+    apiKey: "NotIncludedDueToSecurity",
     authDomain: "alphadook.firebaseapp.com",
     databaseURL: "https://alphadook.firebaseio.com",
-    projectId: "alphadook",
+    projectId: "NotIncludedDueToSecurity",
     storageBucket: "alphadook.appspot.com",
     messagingSenderId: "590705026121",
-    appId: "1:590705026121:web:bd5a225a32f6d9d5668b98",
-    measurementId: "G-BGQ7320LW2"
+    appId: "NotIncludedDueToSecurity",
+    measurementId: "NotIncludedDueToSecurity"
   }
 };
